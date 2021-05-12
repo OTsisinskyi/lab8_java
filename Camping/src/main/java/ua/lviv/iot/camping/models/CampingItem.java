9719c599b184c00ae119e0533089ec0b4e880fa8
OTsisinskyi <alextsisinsky@gmail.com>
2021-05-12 15:03:17 +0300

    deleted the folder idea files, changed the name of enamu, took away the target folder

--- Camping/src/main/java/ua/lviv/iot/camping/models/CampingItem.java
+++ Camping/src/main/java/ua/lviv/iot/camping/models/CampingItem.java
@@ -1,20 +1,2 @@
-package ua.lviv.iot.camping.models;
-
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-public class CampingItem {
-    private String name;
-    private String producer;
-    private Integer weightInGrams;
-    private Integer price;
-
-    public CampingItem(String name, String producer, Integer weightInGrams, Integer price) {
-        this.name = name;
-        this.producer = producer;
-        this.weightInGrams = weightInGrams;
-        this.price = price;
-    }
+package ua.lviv.iot.camping.models;public class CampingItem {
 }
